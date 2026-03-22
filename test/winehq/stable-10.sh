8afc0c67be7c577e893e3262d087a9a164925a44
dewkul <2448694-dewkul@users.noreply.gitlab.com>
2026-03-22 16:00:06 +0000

    test(wine): add tests

---
+++ test/winehq/stable-10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# wine
+check "wine" wine --version
+# check "wine version" bash -c "wine --version | grep 'wine-10'"
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
