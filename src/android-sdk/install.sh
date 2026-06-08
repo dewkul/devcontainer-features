1a34df0420318a95ef7fd3be38c8e862647d04b7
dewkul <2448694-dewkul@users.noreply.gitlab.com>
2026-06-08 05:05:58 +0000

    refac(android-sdk): set sdk tools version

--- src/android-sdk/install.sh
+++ src/android-sdk/install.sh
@@ -2,11 +2,21 @@
 
 # Split by comma
 IFS=',' read -ra SDK_COMPONENTS_ARRAY <<<"$SDKCOMPONENTS"
-ANDROID_HOME=${ANDROIDHOME:-"/home/vscode/.android"}
+export ANDROID_HOME=${ANDROIDHOME:-"/home/vscode/.android"}
 INSTALL_JDK=${INSTALLJDK}
 JDK_VERSION=${JDKVERSION}
+SDK_TOOLS_VERSION=${SDKTOOLSVERSION}
 
 set -e
+
+source ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-extra/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.6"
 
 check_packages() {
     if ! dpkg -s "$@" >/dev/null 2>&1; then
@@ -20,15 +30,6 @@ check_packages curl unzip xz-utils zip wget
 
 # Install JDK via java features
 if [[ "${INSTALL_JDK}" = "true" ]] && ! /usr/local/sdkman/candidates/java/current/bin/java --version > /dev/null 2>&1; then
-    source ./library_scripts.sh
-
-    # nanolayer is a cli utility which keeps container layers as small as possible
-    # source code: https://github.com/devcontainers-extra/nanolayer
-    # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
-    # and if missing - will download a temporary copy that automatically get deleted at the end
-    # of the script
-    ensure_nanolayer nanolayer_location "v0.5.6"
-
     $nanolayer_location \
         install \
         devcontainer-feature \
@@ -37,16 +38,16 @@ if [[ "${INSTALL_JDK}" = "true" ]] && ! /usr/local/sdkman/candidates/java/curren
 fi
 
 # Setup SDK tools
-ANDROID_SDK_URL="https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip"
+ANDROID_SDK_URL="https://dl.google.com/android/repository/commandlinetools-linux-${SDK_TOOLS_VERSION}_latest.zip"
 
 wget -q "$ANDROID_SDK_URL" -O /tmp/cmdline-tools.zip
 unzip -oq /tmp/cmdline-tools.zip -d /tmp
 
-mkdir -p $ANDROID_HOME/cmdline-tools/latest
-mv /tmp/cmdline-tools/* $ANDROID_HOME/cmdline-tools/latest/ 2>/dev/null || true
+mkdir -p $ANDROID_HOME/cmdline-tools/${SDK_TOOLS_VERSION}
+mv /tmp/cmdline-tools/* $ANDROID_HOME/cmdline-tools/${SDK_TOOLS_VERSION}/ 2>/dev/null || true
 
 # install SDK components
-yes | ${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager --licenses && \
-        ${ANDROID_HOME}/cmdline-tools/latest/bin/sdkmanager "${SDK_COMPONENTS_ARRAY[@]}"
+yes | ${ANDROID_HOME}/cmdline-tools/${SDK_TOOLS_VERSION}/bin/sdkmanager --licenses && \
+        ${ANDROID_HOME}/cmdline-tools/${SDK_TOOLS_VERSION}/bin/sdkmanager ${SDK_COMPONENTS_ARRAY[@]}
 
 echo "Done!"
